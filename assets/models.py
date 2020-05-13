9bda2cef35019ecb4a5a1e58bc89da131a83cc9c
Camilo Sanchez <juan.barreiro@correounivalle.edu.co>
2020-05-13 14:25:01 -0500

    agrego campo is_active a los assets

--- assets/models.py
+++ assets/models.py
@@ -4,6 +4,7 @@ class Substation(models.Model):
     name = models.CharField(max_length=15, null=False)
     latitude = models.DecimalField(max_digits=30, decimal_places=20, null=False)
     longitude = models.DecimalField(max_digits=30, decimal_places=20, null=False)
+    is_active = models.BooleanField(default=True)
     # location = models.PointField(null=False, unique=True)
 
 class Transformer(models.Model):
@@ -11,9 +12,11 @@ class Transformer(models.Model):
     latitude = models.DecimalField(max_digits=30, decimal_places=20, null=False)
     longitude = models.DecimalField(max_digits=30, decimal_places=20, null=False)
     substation = models.ForeignKey(Substation, on_delete=models.CASCADE)
+    is_active = models.BooleanField(default=True)
 
 class ElectricMeter(models.Model):
     name = models.CharField(max_length=15, null=False)
     latitude = models.DecimalField(max_digits=30, decimal_places=20, null=False)
     longitude = models.DecimalField(max_digits=30, decimal_places=20, null=False)
     transformer = models.ForeignKey(Transformer, on_delete=models.CASCADE)
+    is_active = models.BooleanField(default=True)
